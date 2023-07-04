dd8cbd08cb0e07487fdc42099ce5aeaf84032daa
Isi <isi.edeoghon@gmail.com>
2023-07-04 23:04:57 +0100

    Backend Updated

---
+++ assets/js/formhandler.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    $("form").submit((function(){
+
+        alert("Your edit has been added to the database!");
+        //href="../../index.html"
+        //  <div style="margin-top:0px;" class="row no-margin" >
+        
+        //<iframe  width="100%" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1595.0821236594422!2d13.060441066902047!3d11.840046073200318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s777%20Estate%20Kano%20Road%2C%20Maiduguri!5e0!3m2!1sen!2sng!4v1680975920234!5m2!1sen!2sng" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    //</div>
+ //   <div class="col-lg-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Basic Table</h4>
+        <p class="card-description"> Add class <code>.table</code>
+        </p>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Profile</th>
+              <th>VatNo.</th>
+              <th>Created</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jacob</td>
+              <td>53275531</td>
+              <td>12 May 2017</td>
+              <td><label class="badge badge-danger">Pending</label></td>
+            </tr>
+            <tr>
+              <td>Messsy</td>
+              <td>53275532</td>
+              <td>15 May 2017</td>
+              <td><label class="badge badge-warning">In progress</label></td>
+            </tr>
+            <tr>
+              <td>John</td>
+              <td>53275533</td>
+              <td>14 May 2017</td>
+              <td><label class="badge badge-info">Fixed</label></td>
+            </tr>
+            <tr>
+              <td>Peter</td>
+              <td>53275534</td>
+              <td>16 May 2017</td>
+              <td><label class="badge badge-success">Completed</label></td>
+            </tr>
+            <tr>
+              <td>Dave</td>
+              <td>53275535</td>
+              <td>20 May 2017</td>
+              <td><label class="badge badge-warning">In progress</label></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+//  </div>
+
+    }))
+
+})
